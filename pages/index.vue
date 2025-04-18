fee3b144c9ae49d951b69265d8cd731d2109b5fe
Eisa khanzadi <esakhanzadi@gmail.com>
2025-04-18 19:55:18 +0330

    create home page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<section class="container mx-auto md:px-10">
+  <IconsLogo/>
+  <p class="text-red-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, cum, eaque id incidunt laboriosam libero modi natus nihil numquam obcaecati odit perspiciatis quaerat quam, quas qui quibusdam quidem soluta vero.</p>
+</section>
+</template>
+
+<style scoped>
+
+</style>
