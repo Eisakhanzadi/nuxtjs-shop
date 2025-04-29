3f2ab467931211ba593cb764f0cf7bb1c943f649
Eisa khanzadi <esakhanzadi@gmail.com>
2025-04-29 23:32:01 +0330

    change url image src

--- components/card/weblog.vue
+++ components/card/weblog.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-const {data, images} = defineProps({
+const {data} = defineProps({
   data: {
     type: Object
   },
-  images: Object
 })
 </script>
 
 <template>
 <article v-if="data" class="overflow-hidden">
   <nuxt-link class="grid gap-2" :to="`/weblog-list/${data.id}/${data.title}`" >
-    <div v-if="images" class="overflow-hidden rounded-xl">
-      <figure ><img :src="images.img" alt=""></figure>
+    <div class="overflow-hidden rounded-xl">
+      <figure ><img :src="data.image?.url" :alt="data.title"></figure>
     </div>
     <div class="category w-fit px-3.5 py-1 rounded-full flex items-center justify-center">
       <nuxt-link :to="`/categories/${data.category.id}/${data.category.name}`" class="  text-sm">{{data.category.name}}</nuxt-link>
