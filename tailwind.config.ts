198131e7f7524e5c047c2bf8fad4bfb73d55b34f
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-03 16:41:04 +0330

    modify container size in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,31 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary:"#f5f5f5" ,
-                color999:"#999" ,
+                primary: "#f5f5f5",
+                color999: "#999",
             },
-
+        },
+        container: {
+            center: true, // centers the container
+            padding: '1rem',
+            screens: {
+                DEFAULT: '100%', // full width on all screens
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
+            },
         },
     },
-    plugins: [],
+    // plugins: [
+    //     function ({addComponents}) {
+    //         addComponents({
+    //             '.center-block': {
+    //                 marginLeft: 'auto',
+    //                 marginRight: 'auto',
+    //             }
+    //         });
+    //     }
+    // ]
 }
