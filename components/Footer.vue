936ab99e8f2e5056cf30e65a311799c1e9278f7c
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-06 12:16:42 +0330

    connect footer api

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,58 +11,80 @@ const shopInfo = computed(() => store.getShopInfo)
 </script>
 
 <template>
-<footer>
-  <section class="container  mx-auto md:px-10 py-5 grid grid-cols-2 gap-x-2 items-center gap-y-5" >
-    <div class="col-span-1">
-      <h5>عضویت در خبرنامه</h5>
-      <p> با اشتراک خبرنامه هفتگی ما از تخفیف ها , اخبارها و مطالب با خبر شوید. </p>
-    </div>
-    <div class="col-span-1" >
-      <form action="">
-        <div class="flex items-center border-gray-400 border-b border-x-0 border-t-0  ">
-          <span class="px-1.5" ><Icon name="uil:github" style="color: black" /></span>
-          <input class="border-0 bg-transparent outline-0 w-full" type="text">
-          <button class="bg-black text-white px-4 py-1.5 text-sm">عضویت</button>
-        </div>
-      </form>
-    </div>
-    <div>
-      <h6>شماره تماس و ایمیل :</h6>
-      <p></p>/
-      <p></p>
-    </div>
-    <div class="flex flex-col gap-2">
-      <div v-if="groupBy && groupBy?.social" class="flex justify-between items-center">
-        <div><nuxt-link to="/">برند شما</nuxt-link></div>
-        <ul class="flex gap-2 items-center justify-end">
-          <li v-for="item in groupBy?.social" :key="item.id">
-            <nuxt-link class="text-sm" to="item.value">{{item.label}}</nuxt-link>
-          </li>
-        </ul>
+  <footer class="pb-10 pt-20">
+    <section class="container  mx-auto grid grid-cols-2 gap-x-2 items-center gap-y-10">
+      <div class="col-span-1">
+        <h5 class="font-bold text-sm">عضویت در خبرنامه</h5>
+        <p class="mt-2 text-sm"> با اشتراک خبرنامه هفتگی ما از تخفیف ها , اخبارها و مطالب با خبر شوید. </p>
       </div>
-      <div class="flex justify-between items-center">
-        <h5>دسترسی سریع</h5>
-        <nav>
-          <ul class="flex items-center gap-3" >
-            <li><nuxt-link to="/">رویه ارسال کالا</nuxt-link></li>
-            <li><nuxt-link to="/">شیوه پرداخت</nuxt-link></li>
-            <li><nuxt-link to="/">قوانین و مقررات</nuxt-link></li>
+      <div class="col-span-1">
+        <form action="">
+          <div class="flex items-center border-gray-400 border-b border-x-0 border-t-0  ">
+            <label class="cursor-pointer -mb-1" for="email-newspaper"><span class="px-1.5 "><Icon name="uil:github" style="color: black"/></span></label>
+            <input id="email-newspaper" class="border-0 bg-transparent outline-0 w-full placeholder:text-sm placeholder:text-gray-400" placeholder="لطفا ایمیل خود را وارد کنید " type="text">
+            <button class="bg-black text-white px-4 py-1.5 text-sm">عضویت</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h6 class="text-color-999 font-bold">شماره تماس و ایمیل :</h6>
+        <p class="mt-2 text-sm">
+          <nuxt-link
+              :to="`tel:${shopInfo?.mobile[0]?.value}`"
+              v-if="shopInfo?.mobile?.length && shopInfo?.mobile[0]?.value"
+          >
+            {{ shopInfo.mobile[0].value }}
+          </nuxt-link>
+          /
+          <nuxt-link
+              :to="`mailto:${shopInfo.email[0].value}`"
+              v-if="shopInfo?.email?.length && shopInfo?.email[0]?.value"
+          >
+                {{ shopInfo.email[0].value }}
+          </nuxt-link>
+        </p>
+
+      </div>
+      <div class="flex flex-col gap-2">
+        <div v-if="data && data?.social" class="flex justify-between items-center">
+          <div>
+            <nuxt-link to="/"><img v-if="shopInfo?.logo?.length && shopInfo?.logo[0]?.value" :src="`${baseUrlTwo}${shopInfo?.logo[0]?.url}`" alt="اطلس مد"></nuxt-link>
+          </div>
+          <ul class="flex gap-2 items-center justify-end">
+            <li v-for="item in data?.social"  :key="item.id">
+              <nuxt-link class="text-sm" :to="item.value">{{ item.label }}</nuxt-link>
+            </li>
           </ul>
-        </nav>
+        </div>
+        <div class="flex justify-between items-center">
+          <h5 class="text-dark font-bold">دسترسی سریع :</h5>
+          <nav>
+            <ul class="flex items-center gap-3">
+              <li>
+                <nuxt-link to="/">رویه ارسال کالا</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">شیوه پرداخت</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">قوانین و مقررات</nuxt-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
-    <div class="address">
-      <h6>آدرس فروشگاه</h6>
-      <p>استان گلستان شهرستان گرگان بلوار گلشهر </p>
-    </div>
-    <div class="design border border-x-0 border-b border-t-gray-300 py-5">
-      <p>
-        <span class="text-color999"> تمامی حقوق این سایت مربوط به سایت برند شما می باشد . </span>
-        <span>عیسی خانزادی</span>
-      </p>
-    </div>
-  </section>
-</footer>
+      <div v-if="shopInfo?.address" class="address">
+        <h6 class="text-color-999 font-bold">آدرس فروشگاه :</h6>
+        <p class="text-sm mt-2" v-if="shopInfo?.address?.length && shopInfo?.address[0]?.value"> {{ shopInfo.address[0].value }} </p>
+      </div>
+      <div class="design border border-x-0 border-b border-t-gray-300 py-5">
+        <p class="text-sm">
+          <span v-if="shopInfo?.title?.length && shopInfo?.title[0]?.value" class="text-color999"> تمامی حقوق این سایت مربوط به سایت <strong >{{shopInfo?.title[0]?.value}}</strong> شما می باشد . </span>
+          <span class="text-xs font-medium">عیسی خانزادی</span>
+        </p>
+      </div>
+    </section>
+  </footer>
 </template>
 
 <style scoped>
