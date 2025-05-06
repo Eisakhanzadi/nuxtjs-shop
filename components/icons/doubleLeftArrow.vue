bf7f84421a5a097b47c58d52f0ae909de000f56a
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-06 12:09:20 +0330

    create icons

---
+++ components/icons/doubleLeftArrow.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg data-v-f36d028e="" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-double-left mr-1"><path data-v-f36d028e="" fill-rule="evenodd" d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path><path data-v-f36d028e="" fill-rule="evenodd" d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path></svg>
+</template>
