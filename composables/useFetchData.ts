a1943530d2940e50e1523b0e85ad4767b6c7a72e
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-06 12:18:20 +0330

    modify use fetch

--- composables/useFetchData.ts
+++ composables/useFetchData.ts
@@ -1,7 +1,10 @@
-export default async function useFetchData(link:string){
+export default async function useFetchData(link:string ,method:string='GET' , body?:{[key:string]:any}, secondBaseUrl?: string) {
     const {public: {baseUrl},serverBaseUrl} = useRuntimeConfig()
 
-    const response  = await $fetch(`${baseUrl}${link}`)
-    const data  = await response
+    const response  = await $fetch(`${secondBaseUrl?.length??baseUrl}${link}` , {
+        method ,
+        body
+    })
+    const data = await response
     return data
 }
