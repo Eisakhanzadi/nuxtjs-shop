4572082bcb907af5949194cec5a5301c0c84b32c
eisa-khanzadi <esakhanzadi@gmail.com>
2025-05-07 01:06:51 +0330

    add skeleton loader

---
+++ components/loader/skeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="skeleton"></div>
+</template>
+<script setup>
+let {bgColor}=defineProps({
+  bgColor:{
+    type:String,
+    default:"linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%)"
+  }
+})
+</script>
+<style scoped>
+.skeleton{
+  background:  v-bind(bgColor);
+  border-radius: 25px;
+  background-size: 200% 100%;
+  animation: 1.5s shine linear infinite;
+}
+@keyframes shine {
+  to {
+    background-position-x: -200%;
+  }
+}
+</style>
