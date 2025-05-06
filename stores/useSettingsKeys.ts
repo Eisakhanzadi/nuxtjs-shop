1371bc1165ad001022d36fbfeb0f4737ee06e5e5
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-06 12:07:29 +0330

    create function for settings info

--- stores/useSettingsKeys.ts
+++ stores/useSettingsKeys.ts
@@ -1,10 +1,12 @@
 import {defineStore} from 'pinia'
 import useFetchData from "~/composables/useFetchData";
+import groupBy from "~/composables/groupBy";
 
 export const useGetSettingsStore = defineStore('getSettingsStore', {
     state: () => {
         return {
             settings: [],
+            shopInfo:[],
             loading: false,
             filterByGroup: []
         }
@@ -12,7 +14,11 @@ export const useGetSettingsStore = defineStore('getSettingsStore', {
     getters: {
         getSetting(state) {
             return state.settings
-        }
+        },
+        getShopInfo(state) {
+            return state.shopInfo
+        },
+
     },
     // could also be defined as
     // state: () => ({ count: 0 })
@@ -21,12 +27,19 @@ export const useGetSettingsStore = defineStore('getSettingsStore', {
             try {
                 this.loading = true
                 this.settings = await useFetchData('settings')
-                this.filterByGroup = groupBy(this.settings ,'group')
+                this.filterByGroup = groupBy(this.settings, 'group')
+                console.log(this.filterByGroup)
+                this.shopInfoGroup()
             } catch (e) {
                 console.log(e)
             } finally {
                 this.loading = false
             }
         },
+        shopInfoGroup(){
+            this.shopInfo = groupBy(this.filterByGroup?.site, 'name')
+            console.log(this.shopInfo)
+        }
+
     },
 })
