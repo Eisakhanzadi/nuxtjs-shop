872d4ae1ee82387df0fa71604ac9bf2ca611d7b7
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-07 15:53:35 +0330

    add request products in products-page

--- pages/product-category/index.vue
+++ pages/product-category/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import {useProductsStore} from "~/stores/products";
 
+const store = useProductsStore()
+onBeforeMount(async ()=>{
+ await store.fetchProducts()
+})
+const products = computed(()=>store.getProducts)
 </script>
 
 <template>
-<section class="grid grid-cols-4 gap-5">
+<section class="grid grid-cols-4 gap-5" v-if="products">
   <main class="col-span-3">
 
   </main>
