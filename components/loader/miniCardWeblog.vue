7670893f6da6f4f0881b3178a21b67f7dcd4be8a
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-07 15:19:34 +0330

    create miniCardWeblog loader

---
+++ components/loader/miniCardWeblog.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <div class="flex gap-1.5">
+      <loader-skeleton class="w-24 h-16 !rounded-lg"/>
+      <div class="grid gap-2 w-full py-1">
+        <loader-skeleton class="w-full h-3.5 !rounded"/>
+        <div class="flex gap-2 w-full justify-between">
+          <loader-skeleton class="w-1/3 h-3 !rounded"/>
+          <loader-skeleton class="w-1/3 h-3 !rounded"/>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
