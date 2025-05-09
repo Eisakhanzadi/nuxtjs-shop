4113bb8794aec53ee0398001a6edc99f4e1242b2
eisa-khanzadi <esakhanzadi@gmail.com>
2025-05-10 01:06:49 +0330

    add static image for description colors in product.vue page

---
+++ components/modal/backdrop.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const emit = defineEmits(['closeModal'])
+const {location} = defineProps({
+  location: {
+    type: String,
+    required: false,
+    default: "items-center"
+  }
+})
+onMounted(() => process.client ? document.body.classList.add('overflow-hidden') : "")
+onBeforeUnmount(() => process.client ? document.body.classList.remove('overflow-hidden') : "")
+
+function closeModal() {
+  console.log('closeModal')
+  emit('closeModal')
+}
+</script>
+
+<template>
+    <div @click.self="closeModal"
+         class="fixed top-0 left-0 w-full h-full flex justify-center p-2 bg-[rgba(0,0,0,0.3)] z-[500]"
+         :class="`${location}`">
+
+      <div class="bg-white rounded-md overflow-hidden">
+        <slot/>
+      </div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
