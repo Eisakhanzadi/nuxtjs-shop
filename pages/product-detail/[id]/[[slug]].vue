eaa8b52b6d401fbf8b0077654cbe5fd1410f219a
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-10 15:52:08 +0330

    create productDetail page

---
+++ pages/product-detail/[id]/[[slug]].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const breadcrumb = [
+  {
+    name:"محصولات",
+    url:"/product-category"
+  },
+  {
+    name:"",
+    url:""
+  },
+]
+const store = useProductsStore()
+</script>
+
+<template>
+<section>
+  <LazyBreadcrumb :data="breadcrumb" />
+</section>
+</template>
+
+<style scoped>
+
+</style>
