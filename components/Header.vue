c4c537eb7d13f23dda7dcf99a0b1f57d5baa1086
Eisakhanzadi <esakhanzadi@gmail.com>
2025-05-10 15:48:48 +0330

    add search modal in header

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,55 @@
 <script setup lang="ts">
-const {public:{baseUrl}} = useRuntimeConfig()
+import {SwiperSlide} from "swiper/vue";
+import {useProductsStore} from "#imports";
+
+const storeProducts = useProductsStore()
+
+const {public: {baseUrl}} = useRuntimeConfig()
+
+onBeforeMount(async () => {
+  await storeProducts.fetchNewProducts()
+
+})
+
+const newProducts = computed(() => storeProducts.getNewProducts)
 const {data} = await useFetch(`${baseUrl}/header`)
-const menus = ref<{[key:string]:any}[]>(data.value.menus)
+const menus = ref<{ [key: string]: any }[]>(data.value.menus)
 const header = ref<HTMLHeadElement>()
+const showSearchBox = ref(false)
 console.log(menus)
-  if (process.client) {
-    window.addEventListener("scroll", () => {
-      let scrollTop:string|number = window.scrollY;
-      if (scrollTop > 250) {
-        if (!header.value?.classList.contains('active-menu')) {
-          header.value?.classList.add('active-menu');
-        }
-      } else {
-        if (header.value?.classList.contains('active-menu')) {
-          header.value?.classList.remove('active-menu');
-        }
+if (process.client) {
+  window.addEventListener("scroll", () => {
+    let scrollTop: string | number = window.scrollY;
+    if (scrollTop > 250) {
+      if (!header.value?.classList.contains('active-menu')) {
+        header.value?.classList.add('active-menu');
       }
-    })
-  }
+    } else {
+      if (header.value?.classList.contains('active-menu')) {
+        header.value?.classList.remove('active-menu');
+      }
+    }
+  })
+}
+const sliderConfig = {
+  '300': {
+    slidesPerView: 1.5,
+    spaceBetween: 15,
+  },
+  '400': {
+    slidesPerView: 2.3,
+    spaceBetween: 10,
+  },
+  '600': {
+    slidesPerView: 3,
+    spaceBetween: 15,
+  },
+  '900': {
+    slidesPerView: 4,
+    spaceBetween: 20,
+  },
+}
+
 </script>
 
 <template>
@@ -34,8 +66,8 @@ console.log(menus)
             <ul class="flex gap-3">
               <li v-for="item in menus" :key="item.name">
 
-                <link-resolver :item="item" >
-                  <span >{{ item.title }}</span>
+                <link-resolver :item="item">
+                  <span>{{ item.title }}</span>
                   <span class="relative top-0.5" v-if="item.children?.length"><icons-angle/></span>
                 </link-resolver>
                 <div v-if="item.children?.length" class="mega-menu-container absolute top-20 right-0 w-full z-50  ">
@@ -48,7 +80,7 @@ console.log(menus)
         <div>
           <ul class="flex items-center gap-2">
             <li class="flex items-center justify-center px-2.5 border-2 border-y-0 border-r-0 border-l-solid border-l-gray-100 ">
-              <button>
+              <button @click="showSearchBox = !showSearchBox">
                 <icons-search/>
               </button>
             </li>
@@ -71,48 +103,85 @@ console.log(menus)
         </div>
       </div>
     </section>
+    <transition-group name="show-modal">
+      <modal-backdrop @close-modal="showSearchBox = false" v-if="showSearchBox" classes="rounded-lg p-10">
+        <div class="w-full ">
+          <form-search/>
+          <div class="mt-5 pt-5" v-if="newProducts?.length">
+            <h4 class="text-base mb-7 font-bold">محصولات پیشنهادی</h4>
+            <slider :breakpoints="sliderConfig">
+              <swiper-slide v-for="item in newProducts" :key="item.id">
+                <card-product :product="item"/>
+              </swiper-slide>
+            </slider>
+          </div>
+        </div>
+      </modal-backdrop>
+    </transition-group>
   </header>
 </template>
 
-<style scoped >
-header{
+<style scoped>
+header {
   background-color: var(--bg-primary);
 }
-nav{
-  ul{
-    li{
-      a{
+
+nav {
+  ul {
+    li {
+      a {
 
       }
-      .mega-menu-container{
+
+      .mega-menu-container {
         visibility: hidden;
         opacity: 0;
         transition: 0.5s;
         transform: translateY(20px);
       }
     }
-    li:hover  .mega-menu-container{
+
+    li:hover .mega-menu-container {
       visibility: visible;
       opacity: 1;
       transform: translateY(-12px);
     }
   }
 }
-.active-menu{
+
+.active-menu {
   position: fixed;
   top: 0;
   right: 0;
   width: 100%;
-  animation: menu 1s 1 forwards ease ;
+  animation: menu 1s 1 forwards ease;
   z-index: 100;
   background-color: white;
-  box-shadow: 3px 0 5px 2px rgba(0,0,0,.08);
+  box-shadow: 3px 0 5px 2px rgba(0, 0, 0, .08);
 }
+
 @keyframes menu {
-  from{
+  from {
     transform: translateY(-100%);
-  }to{
-     transform: translateY(0%);
-   }
+  }
+  to {
+    transform: translateY(0%);
+  }
+}
+
+.show-modal-enter-active{
+  animation: show-modal 0.3s forwards ease ;
+}
+.show-modal-leave-active {
+  animation: show-modal 0.3s reverse forwards ease ;
+}
+
+@keyframes show-modal {
+  from{
+    opacity: 0;
+  }
+  to{
+    opacity: 1;
+  }
 }
 </style>
---
+++ components/form/search.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const searchParams = ref<string>("")
+const emit = defineEmits(['searchItem'])
+const timer = ref()
+async function searchByDelay() {
+  clearTimeout(timer.value)
+  timer.value = setTimeout(() => {
+    if (searchParams.value.length > 0) {
+      // fetchSearchWeblog()
+      emit('searchItem' , searchParams.value)
+    }
+  }, 500)
+}
+
+</script>
+
+<template>
+  <div class="search-box ">
+    <form action="" @submit.prevent="">
+      <div class="input-group border  overflow-hidden flex items-center">
+        <input @input="searchByDelay" v-model="searchParams" type="text" placeholder="دنبال چی میگردی ... ؟"
+               class="w-full text-sm p-2 outline-0 bg-inherit placeholder:text-xs text-center caret-gray-600">
+        <button class="p-1.5">
+          <icons-search/>
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
