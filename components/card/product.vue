0b23ded014a845cfcecff51390b696ac98a0b13a
eisa-khanzadi <esakhanzadi@gmail.com>
2025-05-11 01:59:35 +0330

    complete product cart

--- components/card/product.vue
+++ components/card/product.vue
@@ -1,28 +1,47 @@
 <script setup lang="ts">
-const {product, image} = defineProps({
+const {product} = defineProps({
   product: {
     type: Object,
     required: true
   },
-  image: {
-    type: Object
-  }
-
 })
 </script>
 
 <template>
-  <article v-if="Object.keys(product).length > 0" class="">
+  <article v-if="product && Object.keys(product).length > 0" class="group">
     <nuxt-link :to="`/product-detail/${product.id}/${product.title}`">
-      <div>
-        <figure>
-          <img class="hidden lg:inline-block w-full" :src="product?.major_image?.conversions?.lg" :alt="product.title">
-          <img class="inline-block lg:hidden w-full" :src="product?.major_image?.conversions?.md" :alt="product.title">
-        </figure>
+      <div class="relative overflow-hidden">
+        <div class="relative">
+          <figure class="opacity-100  group-hover:opacity-0 transition ease-linear duration-200  ">
+            <img class="hidden lg:inline-block w-full" :src="product?.major_image?.conversions?.lg" :alt="product.title">
+            <img class="inline-block lg:hidden w-full" :src="product?.major_image?.conversions?.md" :alt="product.title">
+          </figure>
+          <figure class=" opacity-0  group-hover:opacity-100 absolute inset-0 w-full h-full transition ease duration-200" v-if="product?.images?.length">
+            <img :src="product?.images[product?.images?.length-1].url" :alt="product.title">
+          </figure>
+        </div>
+        <div class="hidden md:flex w-fit group-hover:translate-x-0 transition ease-linear duration-300 flex-col items-center justify-center absolute top-1 right-1 translate-x-full">
+          <ul>
+            <li class="mt-1 cursor-default scale-[0.8]" v-for="item in 5" :key="item">
+              <icons-star />
+            </li>
+          </ul>
+          <button class="p-1 mt-2"><icons-heart/></button>
+        </div>
+        <div class="hidden md:flex w-full group-hover:translate-y-0 transition ease duration-300 flex-col items-center justify-center absolute bottom-2 translate-y-[50px]" >
+          <button class="text-center text-xs lg:text-sm bg-white py-2 w-1/2 rounded-lg">
+            مشاهده بیشتر
+          </button>
+        </div>
       </div>
-      <div>
+      <div class="overflow-hidden ">
+        <div class="flex color-range flex-wrap gap-1 items-center w-full overflow-x-auto py-2" v-if="product?.color_ranges?.length">
+          <figure class="p-0.5 overflow-hidden !border-1 border-black" v-for="item in product.color_ranges">
+            <img class="w-6 h-6 md:w-8 md:h-8 rounded-full " :src="item.image" :alt="item.title">
+          </figure>
+        </div>
         <div class="title" v-if="product?.title"><h4>{{product.title}}</h4></div>
-        <div class="price">
+        <div class="price mt-2">
           <span class="price-number">{{separatePrice(product.price)}}</span>
           <span class="text-sm mr-1">تومان</span>
         </div>
@@ -32,5 +51,13 @@ const {product, image} = defineProps({
 </template>
 
 <style scoped>
-
+.color-range{
+  figure{
+    img {
+      padding: 3px;
+    border: 1px solid var(--color-999);
+    border-radius: 100%;
+    }
+  }
+}
 </style>
